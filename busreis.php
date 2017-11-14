d15b18f0fdbed2395d9cf9f945ffd20ca3753a4b
PascalMunniks1998 <pmunniks@roc-dev.com>
2017-11-14 21:32:13 +0100

    busreis af

---
+++ busreis.php
@@ -0,0 +1,12 @@
+<?php
+$leeftijd = 12;
+
+if ($leeftijd >= 66){
+    echo 'De busreis kost 5 euro';
+} else if ($leeftijd >= 13){
+    echo 'De busreis kost 10 euro';
+} else if ($leeftijd >= 3){
+    echo 'De busreis kost 5 euro';
+} else {
+    echo 'De busreis is gratis';
+}
--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: User
- * Date: 21-03-17
- * Time: 9:07
- */
-echo "Hallo wereld";
