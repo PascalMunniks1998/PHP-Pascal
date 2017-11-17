a4b6c2cb16cc479732970f5011ac5a555bbbafc8
PascalMunniks1998 <pmunniks@roc-dev.com>
2017-11-17 14:01:41 +0100

    kerstboom af

---
+++ zwemclubs.php
@@ -0,0 +1,16 @@
+<?php
+print("<table border='2'>");
+$alle ["De spartelkuikens"] = 25;
+$alle ["De waterbuffels"] = 32;
+$alle ["Plonsmderin"] = 11;
+$alle ["Bommetje"] = 23;
+foreach ($alle as $zwemclub => $leden) {
+    echo "<tr> <td colspan='2'> " . $zwemclub . "</td>
+        <td colspan='2'>" . $leden . " </td> <br> <td colspan'2'>";
+    $totaal = $leden / 6;
+    for ($i = 0; $i < $totaal; $i++) {
+        echo "<img src='img/zwem.jpg'>";
+    }
+    echo "</td></tr>";
+}
+print("</table>");
