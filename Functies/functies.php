11a9297c9bc391c12ae8d41e99425c217f09fc0d
PascalMunniks1998 <pmunniks@roc-dev.com>
2017-12-20 14:39:00 +0100

    PHP - Functies AF!!!!!

---
+++ Functies/functies.php
@@ -0,0 +1,41 @@
+<?php
+include("header.php");
+$a = 100;
+
+Function celsius_naar_fahrenheit($a) {
+    $b = ($a * 9/5) + 32;
+
+    echo "{$a} Celsius is {$b} fahrenheit";
+
+}
+celsius_naar_fahrenheit($a);
+echo "<br>";
+
+Function delen() {
+    $a = 9;
+    if($a % 3 == 0) {
+        $check = true;
+    }
+    else {
+        $check = false;
+    }
+    return $check;
+}
+if(delen()) {
+
+    echo "Getal is deelbaar door 3";
+}
+else {
+    echo "Het getal is niet deelbaar door 3";
+}
+delen();
+echo "<br>";
+Function terug_string() {
+    $brengterug = "Dit is een omgekeerde string";
+    echo strrev ("$brengterug");
+
+    return $brengterug;
+}
+terug_string();
+include("footer.php");
+?>
---
+++ Functies/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>PHP-Pascal</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+<body>
+<header>
+    <nav>
+        <ul>
+            <li><a href="functies.php">Main</a></li>
+        </ul>
+    </nav>
+</header>
+
+<section>
